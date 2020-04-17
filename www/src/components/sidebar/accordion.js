26b1765af19f126902f4b305187110f642ab016f
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-04-17 16:19:12 -0700

    Refactor accordion

--- www/src/components/sidebar/accordion.js
+++ www/src/components/sidebar/accordion.js
@@ -42,135 +42,109 @@ const ItemWithSubitems = ({
   )
 }
 
-class Accordion extends React.Component {
-  constructor(props) {
-    super(props)
+export default function Accordion({
+  itemRef,
+  activeItemLink,
+  activeItemParents,
+  isActive,
+  isParentOfActiveItem,
+  item,
+  location,
+  onLinkClick,
+  onSectionTitleClick,
+  openSectionHash,
+  isSingle,
+  disableAccordions,
+}) {
+  const uid = `item_${item.link.replace("/", "_")}`
+  const isExpanded = openSectionHash[item.title] || disableAccordions
 
-    this.state = {
-      uid: (`` + Math.random()).replace(/\D/g, ``),
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick(...args) {
-    if (this.props.onLinkClick) {
-      this.props.onLinkClick(...args)
-    }
-
-    if (this.props.onSectionTitleClick) {
-      this.props.onSectionTitleClick(...args)
-    }
-  }
-
-  render() {
-    const {
-      itemRef,
-      activeItemLink,
-      activeItemParents,
-      isActive,
-      isParentOfActiveItem,
-      item,
-      location,
-      onLinkClick,
-      onSectionTitleClick,
-      openSectionHash,
-      isSingle,
-      disableAccordions,
-    } = this.props
-    const uid = `item_` + this.state.uid
-    const isExpanded = openSectionHash[item.title] || disableAccordions
-
-    return (
-      <li
-        sx={{
-          bg:
-            (isParentOfActiveItem && item.level === 0) ||
-            (isActive && item.level === 0)
-              ? `sidebar.activeSectionBackground`
-              : false,
-          position: `relative`,
-          transition: t =>
-            `all ${t.transition.speed.fast} ${t.transition.curve.default}`,
-          mt: t =>
-            item.level === 0 && disableAccordions && !isSingle
-              ? `${t.space[4]} !important`
-              : false,
-          ...(item.level === 0 &&
-            !isSingle && {
-              "::before": {
-                content: `" "`,
+  return (
+    <li
+      sx={{
+        bg:
+          (isParentOfActiveItem && item.level === 0) ||
+          (isActive && item.level === 0)
+            ? `sidebar.activeSectionBackground`
+            : false,
+        position: `relative`,
+        transition: t =>
+          `all ${t.transition.speed.fast} ${t.transition.curve.default}`,
+        mt: t =>
+          item.level === 0 && disableAccordions && !isSingle
+            ? `${t.space[4]} !important`
+            : false,
+        ...(item.level === 0 &&
+          !isSingle && {
+            "::before": {
+              content: `" "`,
+              position: `absolute`,
+              borderTopWidth: `1px`,
+              borderTopStyle: `solid`,
+              borderColor: `ui.border`,
+              left: t =>
+                (isParentOfActiveItem && isExpanded) || (isActive && isExpanded)
+                  ? 0
+                  : t.space[6],
+              right: 0,
+              top: 0,
+            },
+            ":after": {
+              top: `auto`,
+              bottom: -1,
+            },
+          }),
+      }}
+    >
+      <ItemWithSubitems
+        itemRef={itemRef}
+        activeItemLink={activeItemLink}
+        activeItemParents={activeItemParents}
+        isActive={isActive}
+        isExpanded={isExpanded}
+        isParentOfActiveItem={isParentOfActiveItem}
+        item={item}
+        onLinkClick={onLinkClick}
+        onSectionTitleClick={onSectionTitleClick}
+        uid={uid}
+        disableAccordions={disableAccordions}
+      />
+      {isExpanded && (
+        <ul
+          id={uid}
+          sx={{
+            listStyle: `none`,
+            margin: 0,
+            position: `relative`,
+            ...(item.ui === `steps` && {
+              "&:after": {
+                backgroundColor: `ui.border`,
+                bottom: 0,
+                content: `''`,
+                left: 27,
                 position: `absolute`,
-                borderTopWidth: `1px`,
-                borderTopStyle: `solid`,
-                borderColor: `ui.border`,
-                left: t =>
-                  (isParentOfActiveItem && isExpanded) ||
-                  (isActive && isExpanded)
-                    ? 0
-                    : t.space[6],
-                right: 0,
                 top: 0,
-              },
-              ":after": {
-                top: `auto`,
-                bottom: -1,
+                width: 1,
               },
             }),
-        }}
-      >
-        <ItemWithSubitems
-          itemRef={itemRef}
-          activeItemLink={activeItemLink}
-          activeItemParents={activeItemParents}
-          isActive={isActive}
-          isExpanded={isExpanded}
-          isParentOfActiveItem={isParentOfActiveItem}
-          item={item}
-          onLinkClick={onLinkClick}
-          onSectionTitleClick={onSectionTitleClick}
-          uid={uid}
-          disableAccordions={disableAccordions}
-        />
-        {isExpanded && (
-          <ul
-            id={uid}
-            sx={{
-              listStyle: `none`,
-              margin: 0,
-              position: `relative`,
-              ...(item.ui === `steps` && {
-                "&:after": {
-                  backgroundColor: `ui.border`,
-                  bottom: 0,
-                  content: `''`,
-                  left: 27,
-                  position: `absolute`,
-                  top: 0,
-                  width: 1,
-                },
-              }),
-            }}
-          >
-            {item.items.map(subitem => (
-              <Item
-                activeItemLink={activeItemLink}
-                activeItemParents={activeItemParents}
-                item={subitem}
-                key={subitem.title}
-                location={location}
-                onLinkClick={onLinkClick}
-                isExpanded={isExpanded}
-                onSectionTitleClick={onSectionTitleClick}
-                openSectionHash={openSectionHash}
-                ui={item.ui}
-              />
-            ))}
-          </ul>
-        )}
-      </li>
-    )
-  }
+          }}
+        >
+          {item.items.map(subitem => (
+            <Item
+              activeItemLink={activeItemLink}
+              activeItemParents={activeItemParents}
+              item={subitem}
+              key={subitem.title}
+              location={location}
+              onLinkClick={onLinkClick}
+              isExpanded={isExpanded}
+              onSectionTitleClick={onSectionTitleClick}
+              openSectionHash={openSectionHash}
+              ui={item.ui}
+            />
+          ))}
+        </ul>
+      )}
+    </li>
+  )
 }
-
-export default Accordion
