b4adca359d07fb7aae87265574a2d2392d879928
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-04-21 18:35:14 -0700

    guide-list

--- www/src/components/guide-list.js
+++ www/src/components/guide-list.js
@@ -1,33 +1,6 @@
 import React from "react"
 import { Trans } from "@lingui/macro"
 
-import docsHierarchy from "../data/sidebars/doc-links.yaml"
-import tutorialHierarchy from "../data/sidebars/tutorial-links.yaml"
-import contributingHierarchy from "../data/sidebars/contributing-links.yaml"
-
-// Search through tree, which may be 2, 3 or more levels deep
-const childItemsBySlug = (
-  docsHierarchy,
-  tutorialHierarchy,
-  contributingHierarchy,
-  slug
-) => {
-  let result
-
-  const iter = a => {
-    if (a.link === slug) {
-      result = a
-      return true
-    }
-    return Array.isArray(a.items) && a.items.some(iter)
-  }
-
-  docsHierarchy.some(iter)
-  tutorialHierarchy.some(iter)
-  contributingHierarchy.some(iter)
-  return result && result.items
-}
-
 const GuideList = ({ items = [] }) => {
   const subitemList = items.map((subitem, i) => (
     <li key={i}>
