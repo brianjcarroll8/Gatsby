de70f911ec2de27bfab5bf9a9a6339ab10788edc
LekoArts <lekoarts@gmail.com>
2020-04-30 10:48:53 +0200

    Revert "copy theme contents to starter folder"
    This reverts commit 1fbf01141b3b94539b0254d9274d33603cf3d1a6.

--- starters/gatsby-starter-course-theme/gatsby-config.js
+++ starters/gatsby-starter-course-theme/gatsby-config.js
@@ -1,44 +1,8 @@
-const withDefaults = require(`./utils/default-options`);
-const options = withDefaults({});
-
 module.exports = {
-  siteMetadata: {
-    title: `Course Site`,
-    description: `you can see a course here!`,
-    author: `Madalyn Parker`,
-  },
-  plugins: [
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-theme-ui`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: options.coursePath || `content/course`,
-        name: options.coursePath || `content/course`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: options.assetPath || `content/assets`,
-        name: options.assetPath || `content/assets`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1035,
-              sizeByPixelDensity: true,
-            },
-          },
-        ],
-      },
-    },
-  ],
+	siteMetadata: {
+		title: `Course Site`,
+		description: `you can see a course here!`,
+		author: `Madalyn Parker`,
+	},
+	plugins: [{ resolve: `gatsby-theme-course`, options: {} }],
 };
